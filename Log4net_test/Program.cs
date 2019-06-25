eedea6a990f31932e507b61ef7b528c50dfa1306
Daniel Pérez <daniel.perezpa@hotmail.es>
2019-06-25 12:11:51 +0200

    Added console error log.

--- Log4net_test/Program.cs
+++ Log4net_test/Program.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
 namespace Log4net_test
 {
     class Program
     {
+        private static readonly log4net.ILog log = 
+            log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World");
+
+            log.Error("This is my error");
+
+            Console.ReadLine();
         }
     }
 }
